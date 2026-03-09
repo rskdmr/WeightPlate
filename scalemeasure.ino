6d37190eb4fb4d0da44d760a98597739e14fdb21
anmai <anmai@usc.edu>
2026-03-09 15:37:16 -0700

    Add files via upload

---
+++ scalemeasure.ino
@@ -0,0 +1,45 @@
+/* 
+ Example using the SparkFun HX711 breakout board with a scale
+ 
+ This example demonstrates basic scale output. See the calibration sketch to get the calibration_factor for your
+ specific load cell setup.
+
+ This example code uses bogde's excellent library:"https://github.com/bogde/HX711"
+ bogde's library is released under a GNU GENERAL PUBLIC LICENSE
+
+ The HX711 does one thing well: read load cells. The breakout board is compatible with any wheat-stone bridge
+ based load cell which should allow a user to measure everything from a few grams to tens of tons.
+ Arduino pin 2 -> HX711 CLK
+ 3 -> DAT
+ 5V -> VCC
+ GND -> GND
+
+ The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
+
+*/
+
+#include "HX711.h"
+
+#define calibration_factor -3000.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+
+#define LOADCELL_DOUT_PIN  6 // Pin D6 to DATA
+#define LOADCELL_SCK_PIN  5 // Pin D5 to CLK
+HX711 scale;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("HX711 scale demo");
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
+
+  Serial.println("Readings:");
+}
+
+void loop() {
+  Serial.print("Reading: ");
+  Serial.print(scale.get_units(), 1); //scale.get_units() returns a float
+  Serial.print(" kgs"); //You can change this to kg but you'll need to refactor the calibration_factor
+  Serial.println();
+}
