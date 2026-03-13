8b31e7b2b403790f9210a7b2998958231e1f24f4
anmai <anmai@usc.edu>
2026-03-12 17:57:01 -0700

    Update SeniorDesign.ino
    Swapped signs to match new four sensor setup
    Increased number of decimal points for data

--- SeniorDesign.ino
+++ SeniorDesign.ino
@@ -38,7 +38,7 @@
 
 HX711 scale;
 
-float calibration_factor = -3000; //-7050 worked for my 440lb max scale setup
+float calibration_factor = 7000; //-7050 worked for my 440lb max scale setup
 
 void setup() {
   Serial.begin(9600);
@@ -63,7 +63,7 @@ void loop() {
   scale.set_scale(calibration_factor); //Adjust to this calibration factor
 
   Serial.print("Reading: ");
-  Serial.print(scale.get_units(), 1);
+  Serial.print(scale.get_units(), 3);
   Serial.print(" kgs"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
   Serial.print(" calibration_factor: ");
   Serial.print(calibration_factor);
